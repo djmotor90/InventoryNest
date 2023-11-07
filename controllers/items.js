a0252e9dd15435efab485a56f4a22661a2ed4e70
akullyot <akullyot@gmail.com>
2023-11-06 20:52:28 -0600

    initialized express configuration and began mapping out controller routes

---
+++ controllers/items.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const items = express.Router();
+
+items.get('/',(req,res) => {
+    // Show a table of all items 
+ 
+
+});
+
+items.get('/:id',(req,res) => {
+    // show each individual items
+    
+    
+});
+
+
+
+
+
+
+
+
+
