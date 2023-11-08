a9dd4afe8f49d137da0b4898a8208ffb1709a86e
akullyot <akullyot@gmail.com>
2023-11-08 10:01:27 -0600

    added delivery detail model and mig

---
+++ models/delivery_detail.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Delivery_Detail extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Delivery_Detail.init({
+    delivery_detail_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    delivery_id: DataTypes.STRING,
+    product_id: DataTypes.STRING,
+    quantity: DataTypes.INTEGER,
+    total_price: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Delivery_Detail',
+  });
+  return Delivery_Detail;
+};
