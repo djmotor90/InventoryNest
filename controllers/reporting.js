3f93685746bcadd6183750e73411fa26b8c2e3fe
akullyot <akullyot@gmail.com>
2023-11-14 17:28:19 -0600

    modified your find all customers purchases to work with the new database design

--- controllers/reporting.js
+++ controllers/reporting.js
@@ -62,22 +62,46 @@ router.get('/products', async (req,res) => { // Change the path to '/products'
 // Get all customers
 router.get('/customers', async (req, res) => {
     try {
-      // Search through the queries and find those which match a column name from the customer model
-      const columnNames = Object.keys(Customer.rawAttributes);
-      let whereObject = {};
-      for (let i = 0; i < Object.keys(req.query).length; i++) {
-        if (columnNames.includes(Object.keys(req.query)[i])) {
-          whereObject[Object.keys(req.query)[i]] = req.query(Object.keys(req.query)[i]);
-        }
-      }
-  
-      // Query the customers based on the specified conditions
-      const foundCustomers = await Customer.findAll({
-        where: whereObject,
-      });
+        //annie Note: you dont need this where part, its used only when youre sending a request to filter and or query a result.
+        //here the client is never going to send back a query
+        //first lets populate out our model
+        const allCustomers = await Customer.findAll({
+            attributes: ['customer_id'],
+            include:[
+            {model: Delivery, as: "deliveries",
+                include: {
+                    model: Delivery_Detail, as: "delivery_details",attributes: ['quantity', 'product_id'],
+                        include:{
+                            model: Product, as : "product",
+                            attributes: ['product_sale_price']
+                        }
+            }},
+            ]
+        });
+        // now we map through every customer and find every delivery they have
+        const allPurchasesByCustomer = allCustomers.map(customer => {
+            //this is what we will return. we sum by constantly adding to indvCustomerObj quantity
+            let indvCustomerObj = { customer_id : customer.dataValues.customer_id,
+                                    quantityPurchased: 0,
+                                    amountSpent      : 0
+                                }
+            //each customer can have multiple deliveries
+            customer.dataValues.deliveries.forEach(delivery => {
+                //each delivery can have multiple delivery details
+                delivery.dataValues.delivery_details.forEach(delivery_detail => {
+                    //lets go and add it to our count
+                    indvCustomerObj.quantityPurchased = indvCustomerObj.quantityPurchased + delivery_detail.dataValues.quantity;
+                    //thank god delivery detail to product is one to one so we dont need another nested forEach, we just have 
+                    //to multiply the quantity by the product sale price
+                    indvCustomerObj.amountSpent = indvCustomerObj.amountSpent + (delivery_detail.dataValues.quantity * delivery_detail.dataValues.product.dataValues.product_sale_price);
+                })
+            });
+            //give it back an object of what we care about
+            return indvCustomerObj;
+        });
   
       res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-      res.status(200).json(foundCustomers);
+      res.status(200).json(allPurchasesByCustomer);
     } catch (err) {
       res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
       res.status(500).json(err);
