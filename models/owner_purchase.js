62d5c99b8617aadf530379abfaac7bc3138f281a
akullyot <akullyot@gmail.com>
2023-11-15 08:55:05 -0600

    ah sorry another typo

--- models/owner_purchase.js
+++ models/owner_purchase.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-  static associate(models) {
+  static associate({ Product, Warehouse }) {
     //each purchase entry belongs to one warehouse
     Owner_Purchase.belongsTo(Warehouse,{
       foreignKey:'warehouse_id',
