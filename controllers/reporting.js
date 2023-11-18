8c3f3bc34002d8960e8144e20ca5ce868f18dbc4
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-11-17 23:11:16 -0600

    reporting updated

--- controllers/reporting.js
+++ controllers/reporting.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
-const { Product, Owner, Customer, Delivery, Delivery_Detail } = db; // Import the necessary models
+const { Product, Owner, Customer, Delivery, Delivery_Detail, Warehouse, Transfer } = db; // Import the necessary models
 const { Op, sequelize } = require('sequelize');
 
 // Get all owners
@@ -49,7 +49,7 @@ router.get('/products', async (req,res) => { // Change the path to '/products'
         //you will eventually have to rewrite out the where here dynamically
         const foundProducts = await Product.findAll({
             where: whereObject,
-            attributes: {exclude: ['createdAt', 'updatedAt', 'product_picture_filename']}
+        
         });
         res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
         res.status(200).json(foundProducts);
@@ -85,64 +85,123 @@ router.get('/customers', async (req, res) => {
   });
 
  // Get customer orders with total quantities and amount spent
-router.get('/customerorders', async (req, res) => {
+ router.get('/customerorders', async (req, res) => {
     try {
-      // Populate our model
-      const allCustomers = await Customer.findAll({
-        attributes: ['customer_id', 'customer_first_name', 'customer_last_name'],
-        include: [
-          {
-            model: Delivery,
-            as: "deliveries",
-            include: {
-              model: Delivery_Detail,
-              as: "delivery_details",
-              attributes: ['quantity', 'product_id'],
-              include: {
-                model: Product,
-                as: "product",
-                attributes: ['product_sale_price']
-              }
-            },
-          },
-        ],
-      });
-  
-      // Map through every customer and find every delivery they have
-      const allPurchasesByCustomer = allCustomers.map(customer => {
-        // This is what we will return. We sum by constantly adding to indvCustomerObj quantity
-        let indvCustomerObj = {
-          customer_id: customer.dataValues.customer_id,
-          customer_first_name: customer.dataValues.customer_first_name,
-          customer_last_name: customer.dataValues.customer_last_name,
-          quantityPurchased: 0,
-          amountSpent: 0
-        };
-  
-        // Each customer can have multiple deliveries
-        customer.dataValues.deliveries.forEach(delivery => {
-          // Each delivery can have multiple delivery details
-          delivery.dataValues.delivery_details.forEach(delivery_detail => {
-            // Let's go and add it to our count
-            indvCustomerObj.quantityPurchased = indvCustomerObj.quantityPurchased + delivery_detail.dataValues.quantity;
-            // Thank god delivery detail to product is one to one, so we don't need another nested forEach,
-            // we just have to multiply the quantity by the product sale price
-            indvCustomerObj.amountSpent = indvCustomerObj.amountSpent + (delivery_detail.dataValues.quantity * delivery_detail.dataValues.product.dataValues.product_sale_price);
-          })
+        const allCustomers = await Customer.findAll({
+            include: [
+                {
+                    model: Delivery,
+                    as: "deliveries",
+                    include: {
+                        model: Delivery_Detail,
+                        as: "delivery_details",
+                        attributes: ['quantity', 'product_id'],
+                        include: {
+                            model: Product,
+                            as: "product",
+                            attributes: ['product_sale_price']
+                        }
+                    },
+                },
+            ],
         });
-  
-        // Filter out customers with quantity less than or equal to 0
-        return indvCustomerObj.quantityPurchased > 0 ? indvCustomerObj : null;
-      }).filter(Boolean); // Remove null values
-  
-      res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-      res.status(200).json(allPurchasesByCustomer);
+
+        const allPurchasesByCustomer = allCustomers.map(customer => {
+            let indvCustomerObj = {
+                customer_id: customer.customer_id,
+                customer_first_name: customer.customer_first_name,
+                customer_last_name: customer.customer_last_name,
+                customer_address: customer.customer_address,
+                customer_city: customer.customer_city,
+                customer_state: customer.customer_state,
+                customer_zipcode: customer.customer_zipcode,
+                customer_picture_filename: customer.customer_picture_filename,
+                isSoftDeleted: customer.isSoftDeleted,
+                quantityPurchased: 0,
+                amountSpent: 0
+            };
+
+            customer.deliveries.forEach(delivery => {
+                delivery.delivery_details.forEach(delivery_detail => {
+                    indvCustomerObj.quantityPurchased += delivery_detail.quantity;
+                    indvCustomerObj.amountSpent += delivery_detail.quantity * delivery_detail.product.product_sale_price;
+                });
+            });
+
+            return indvCustomerObj.quantityPurchased > 0 ? indvCustomerObj : null;
+        }).filter(Boolean); // Remove null values
+
+        res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.status(200).json(allPurchasesByCustomer);
     } catch (err) {
-      console.error(err); // Log any errors to the console for debugging
+        console.error(err);
+        res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.status(500).json(err);
+    }
+});
+
+// Get all warehouses
+router.get('/warehouses', async (req, res) => {
+  try {
+      const allWarehouses = await Warehouse.findAll({
+          include: [
+              {
+                  model: Transfer,
+                  as: 'transfers_to',
+                  include: [{ model: Product, as: 'product' }]
+              },
+              {
+                  model: Transfer,
+                  as: 'transfers_from',
+                  include: [{ model: Product, as: 'product' }]
+              }
+          ]
+      });
+
+      const warehousesWithMetrics = allWarehouses.map(warehouse => {
+          let totalProductsTransferredTo = 0;
+          let totalProductsTransferredFrom = 0;
+          let totalTransferAmountTo = 0;
+          let totalTransferAmountFrom = 0;
+
+          warehouse.transfers_to.forEach(transfer => {
+              totalProductsTransferredTo += transfer.transfer_amount;
+              totalTransferAmountTo += transfer.transfer_amount * transfer.product.product_sale_price;
+          });
+
+          warehouse.transfers_from.forEach(transfer => {
+              totalProductsTransferredFrom += transfer.transfer_amount;
+              totalTransferAmountFrom += transfer.transfer_amount * transfer.product.product_sale_price;
+          });
+
+          // Only return the specific fields you want along with the metrics
+          return {
+              warehouse_id: warehouse.warehouse_id,
+              warehouse_name: warehouse.warehouse_name,
+              warehouse_description: warehouse.warehouse_description,
+              warehouse_address: warehouse.warehouse_address,
+              warehouse_city: warehouse.warehouse_city,
+              warehouse_state: warehouse.warehouse_state,
+              warehouse_zipcode: warehouse.warehouse_zipcode,
+              warehouse_capacity: warehouse.warehouse_capacity,
+              isSoftDeleted: warehouse.isSoftDeleted,
+              createdAt: warehouse.createdAt,
+              updatedAt: warehouse.updatedAt,
+              totalProductsTransferredTo,
+              totalProductsTransferredFrom,
+              totalTransferAmountTo,
+              totalTransferAmountFrom
+          };
+      });
+
+      res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+      res.status(200).json(warehousesWithMetrics);
+  } catch (err) {
+      console.error(err);
       res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
       res.status(500).json(err);
-    }
-  });
+  }
+});
  
   router.get('/:id', async (req, res) => {
     try {
@@ -205,5 +264,5 @@ router.get('/customerorders', async (req, res) => {
       res.status(500).json(err);
     }
   });
-  
+ 
 module.exports = router;
